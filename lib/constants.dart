0b06220e79197cf9499e4c444f65ec546e94e942
Hazemzohdy <hazemzohdy820@gmail.com>
2024-08-03 16:10:33 +0300

    Splash view done

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/widgets.dart';
+
+const kPrimaryKey = Color(0xff100B20);
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:booklyapp/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'Features/splash/presentation/views/splash_view.dart';
 
 void main() {
-  runApp( const BooklyApp());
+  runApp(const BooklyApp());
 }
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashView(),
+    return GetMaterialApp(
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryKey,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
     );
   }
 }
