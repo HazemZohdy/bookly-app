4483b61d2c097b4fb608dc30609d08a079ea8281
Hazemzohdy <hazemzohdy820@gmail.com>
2024-08-05 05:44:16 +0300

    refactor inite state & navigate to home

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,11 @@
+import 'package:booklyapp/Features/home/presentation/views/home_view.dart';
 import 'package:booklyapp/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:booklyapp/constants.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -19,16 +22,13 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    );
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 7), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    NavigateToHome();
   }
 
+ 
+
+  
   @override
   void dispose() {
     super.dispose();
@@ -47,4 +47,23 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
+  
+
+ void NavigateToHome() {
+    Future.delayed(const Duration(seconds: 3),(){
+      Get.to(()=> HomeView(),transition: Transition.upToDown,duration: kTranstionDuration,);
+    });
+  }
+
+  void initSlidingAnimation() {
+     animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 7), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/widgets.dart';
 
 const kPrimaryKey = Color(0xff100B20);
+const kTranstionDuration = Duration(milliseconds: 250);
